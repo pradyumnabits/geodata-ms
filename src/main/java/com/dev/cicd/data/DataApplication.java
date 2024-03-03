ecc6ed19da87dcc3c9ded14e40e42a46f53334f9
Pradyumna <2022mt03596@wilp.bits-pilani.ac.in>
2024-03-03 16:52:19 +0530

    github actions workflow

--- src/main/java/com/dev/cicd/data/DataApplication.java
+++ src/main/java/com/dev/cicd/data/DataApplication.java
@@ -4,19 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class DataApplication {
+public class DataApplication {
 
-    private DataApplication() {
-        // Private constructor to prevent instantiation
-    }
-
-    /**
-     * Main method to start the DataApplication.
-     *
-     * @param args Command line arguments.
-     */
-    public static void main(final String[] args) {
-        SpringApplication.run(DataApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DataApplication.class, args);
+	}
 
 }
--- src/main/java/com/dev/cicd/data/controller/DataController.java
+++ src/main/java/com/dev/cicd/data/controller/DataController.java
@@ -8,71 +8,48 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Locale;
 
-/**
- * Controller class for handling data-related requests.
- */
 @RestController
-public final class DataController {
+public class DataController {
 
-    /**
-     * Endpoint for performing a health check.
-     *
-     * @return A string indicating the health status.
-     */
-    @GetMapping("/")
-    public String healthCheck() {
-        return "HEALTH CHECK OK!";
-    }
-
-    /**
-     * Endpoint for retrieving the application version.
-     *
-     * @return A string indicating the application version.
-     */
-    @GetMapping("/version")
-    public String version() {
-        return "The actual version is 1.0.0";
-    }
-
-    /**
-     *  Randomly generated nations.
-     *
-     * @return A JSON array containing nation details.
-     */
-    @GetMapping("/nations")
-    public JsonNode getRandomNations() {
-        var objectMapper = new ObjectMapper();
-        var faker = new Faker(new Locale("en-US"));
-        var nations = objectMapper.createArrayNode();
-        final int numberOfNations = 10;
-        for (var i = 0; i < numberOfNations; i++) {
-            var nation = faker.nation();
-            nations.add(objectMapper.createObjectNode()
-                    .put("nationality", nation.nationality())
-                    .put("capitalCity", nation.capitalCity())
-                    .put("flag", nation.flag())
-                    .put("language", nation.language()));
+        @GetMapping("/")
+        public String healthCheck() {
+                return "HEALTH CHECK OK!";
         }
-        return nations;
-    }
 
-    /**
-     * Randomly generated currencies.
-     *
-     * @return A JSON array containing currency details.
-     */
-    @GetMapping("/currencies")
-    public JsonNode getRandomCurrencies() {
-        var objectMapper = new ObjectMapper();
-        var faker = new Faker(new Locale("en-US"));
-        var currencies = objectMapper.createArrayNode();
-        final int numberOfCurrencies = 20;
-        for (var i = 0; i < numberOfCurrencies; i++) {
-            var currency = faker.currency();
-            currencies.add(objectMapper.createObjectNode()
-                    .put("name", currency.name())
-                    .put("code", currency.code()));
+        @GetMapping("/version")
+        public String version() {
+                return "The actual version is 1.0.0";
         }
-        return currencies;
-    }
+
+        @GetMapping("/nations")
+        public JsonNode getRandomNations() {
+                var objectMapper = new ObjectMapper();
+                var faker = new Faker(new Locale("en-US"));
+                var nations = objectMapper.createArrayNode();
+                for (var i = 0; i < 10; i++) {
+                        var nation = faker.nation();
+                        nations.add(objectMapper.createObjectNode()
+                                .put("nationality", nation.nationality())
+                                .put("capitalCity", nation.capitalCity())
+                                .put("flag", nation.flag())
+                                .put("language", nation.language()));
+                }
+                return nations;
+        }
+
+        @GetMapping("/currencies")
+        public JsonNode getRandomCurrencies() {
+                var objectMapper = new ObjectMapper();
+                var faker = new Faker(new Locale("en-US"));
+                var currencies = objectMapper.createArrayNode();
+                for (var i = 0; i < 20; i++) {
+                        var currency = faker.currency();
+                        currencies.add(objectMapper.createObjectNode()
+                                .put("name", currency.name())
+                                .put("code", currency.code()));
+                }
+                return currencies;
+
+        }
+
 }
